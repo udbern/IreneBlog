87c55e50b57ae7cf6147ccfdc26afb081878475b
udbern <edwinuduakabasi@gmail.com>
2023-07-06 19:22:44 +0100

    mobile menu changes

--- src/component/Hamburger.jsx
+++ src/component/Hamburger.jsx
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { HiMenu, HiMenuAlt3 } from 'react-icons/hi';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { motion } from "framer-motion"
 
 const Hamburger = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   };
+
+  const handleLinkClick = () => {
+    setIsOpen(false);
+  };
+
+  const handleOutsideClick = (e) => {
+    if (menuRef.current && !menuRef.current.contains(e.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -19,33 +38,35 @@ const Hamburger = () => {
 
   return (
     <div className=''>
-      <div className='md:hidden relative'>
+      <div className='md:hidden relative' ref={menuRef}>
         {isOpen ? (
-          <HiMenuAlt3
-            className='cursor-pointer'
-            onClick={handleClick}
-          />
+          <HiMenuAlt3 className='cursor-pointer' onClick={handleClick} />
         ) : (
-          <HiMenu
-            className='cursor-pointer'
-            onClick={handleClick}
-          />
+          <HiMenu className='cursor-pointer' onClick={handleClick} />
         )}
       </div>
       {isOpen && (
-        <motion.div className='absolute w-56 top-[68px] right-0 h-80 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'
-        initial={{y:-250}}
-        animate={{y:0}}>
-          <ul className='grid items-center justify-start mt-20 p-5 space-y-5 text-white'>
+        <motion.div className='absolute w-60 top-16 right-0 h-80 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'
+          initial={{ y: -250 }}
+          animate={{ y: 0 }}>
+          <ul className='grid items-center justify-center p-5 space-y-5 text-white'>
             <li
-              className={`font-body cursor-pointer font-semibold text-gray-100 border-b-2 b border-b-transparent ${pathMatchRoute("/") && "text-white !border-b-gray-100"}`}
-              onClick={() => navigate("/")}
+              className={`font-body cursor-pointer font-semibold text-gray-100 border-b-2 b border-b-transparent ${pathMatchRoute('/') ? 'text-white !border-b-gray-100' : ''
+                }`}
+              onClick={() => {
+                navigate('/');
+                handleLinkClick();
+              }}
             >
               Home
             </li>
             <li
-              className={`cursor-pointer font-semibold text-gray-100 border-b-2 b border-b-transparent ${pathMatchRoute("/Blog") && "!text-white !border-b-gray-100"}`}
-              onClick={() => navigate("/Blog")}
+              className={`cursor-pointer font-semibold text-gray-100 border-b-2 b border-b-transparent ${pathMatchRoute('/Blog') ? '!text-white !border-b-gray-100' : ''
+                }`}
+              onClick={() => {
+                navigate('/Blog');
+                handleLinkClick();
+              }}
             >
               Blog
             </li>
@@ -57,3 +78,5 @@ const Hamburger = () => {
 };
 
 export default Hamburger;
+
+
